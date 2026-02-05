7df239dccdf7cf049f62dd6c47fd3443b571e774
seung-heee <tmdgmldi2595@naver.com>
2026-02-05 17:57:47 +0900

    fix: base-url

--- src/api/axios-instance.ts
+++ src/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import type { AxiosRequestConfig } from 'axios';
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || null;
 
 export const AXIOS_INSTANCE = axios.create({
   baseURL: BASE_URL,
